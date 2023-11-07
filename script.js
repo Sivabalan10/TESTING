52d89f398600ca603d504a552f15db4d6913e3de
Sivabalan10 <135865311+Sivabalan10@users.noreply.github.com>
2023-11-07 16:17:36 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function welcome(){
-	swal({title:"Welcome to Ikebana",
-		text:`In rivers, flowers bloom, a colorful sight,
-	Enactus's eco-journey, making the future bright`,
-		   confirmButtonText:"Continue",
-		   confirmButtonColor: "skyblue",});
-
-}

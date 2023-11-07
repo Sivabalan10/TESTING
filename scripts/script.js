c3726e2e0d4f5a7bb226bf8babed9ffcc3521221
Sivabalan10 <135865311+Sivabalan10@users.noreply.github.com>
2023-11-07 16:18:55 +0530

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function welcome(){
+	swal({title:"Welcome to Ikebana",
+		text:`In rivers, flowers bloom, a colorful sight,
+	Enactus's eco-journey, making the future bright`,
+		   confirmButtonText:"Continue",
+		   confirmButtonColor: "skyblue",});
+
+}
